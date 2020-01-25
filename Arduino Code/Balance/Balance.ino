336cfb82c38ca0dd3f02453bdad279220b1fa21c
Liam Smolenaars <liam.great98@gmail.com>
2020-01-25 14:45:32 -0500

    Update Balance.ino
    Implemented function to compute angle of tilt using accelerometer readings, for the particular orientation the sensor will be mounted

--- Arduino Code/Balance/Balance.ino
+++ Arduino Code/Balance/Balance.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "math.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
+MPU6050 IMU;
+
+int16_t x, y, z;
+float current_angle;
+
+void setup() {
+  Serial.begin(9600);
+  IMU.initialize();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (IMU.testConnection()) {
+    x = IMU.getAccelerationX();
+    y = IMU.getAccelerationY();
+    z = IMU.getAccelerationZ();
+    current_angle = atan2(x, y) * RAD_TO_DEG;
+    Serial.print("Angle: ");
+    Serial.print(current_angle);
+    Serial.println("º");
+  }
+  delay(1000);
 }
